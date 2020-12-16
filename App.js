9c2645d95ee3b9b174b7340919ecbfabafa4a51c
PeterWambua1 <51115886+PeterWambua1@users.noreply.github.com>
2020-12-16 09:54:03 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,45 @@
+import React, {useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+
+  const[state, setState]= useState ({
+    joke: ''
+  })
+
+
+
+
+useEffect( ()=> {
+fetchData();
+}, []);
+
+const fetchData = async ()=> {
+const result= await axios.get('https://api.chucknorris.io/jokes/random');
+console.log(result.data.value );  
+setState({
+  ...state,
+  joke:result.data.value 
+} );
+}
+
+
+
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+      <h1 className="title">Chuck Norris API </h1>
+    </div>
+    </div>
+
+<h3> {state.joke}</h3>
+    </div>
+     
+
+  );
+}
+
+export default App;
